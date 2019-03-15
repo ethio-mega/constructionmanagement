c102d6c36262c3d31fbc3f4db121ce7ecc88e934
Yohannes-Mekonnen <johannesmekonnen@gmail.com>
2019-03-15 05:46:52 -0800

    Update db_functions.php
    brought the db_function back

--- db_functions.php
+++ db_functions.php
@@ -120,6 +120,7 @@ class DB_Functions {
         $result = mysqli_query($connection, "SELECT * FROM bidding_request");
         return $result;
     }
+    
 	
 	 public function getLimitedProcurmentRequests() {
         $connection = $this->db->connect();    
@@ -174,7 +175,145 @@ class DB_Functions {
         $connection = $this->db->connect();            
 		$result = mysqli_query($connection, "UPDATE purchase_requests SET status = 1 WHERE id = '$id'");
 		return $result;
-	}
-}
+    }
+    
+     /*
+        select list of materials which is not approved
+       
+      */   
+      public function requiredMaterial(){
+          $connection = $this->db->connect();
+          $result = mysqli_query($connection,"SELECT * from purchase_requests");
+          return $result;
+      }
+
+     public function acceptRequest($material_desc,$quantity,$name,$unit,$unit_price,$total_price,$remark,$sub_date){
+
+        $dt = new DateTime();
+        $dt = $dt->format('Y-m-d');
+
+         $connection = $this->db->connect(); 
+         mysqli_query($connection,"INSERT into approved_pr values('','$material_desc','$quantity','$dt','$sub_date','$name','$unit','$unit_price','$total_price','$remark')");
+         mysqli_query($connection,"DELETE FROM purchase_requests WHERE material_description = '$material_desc'"); 
+
+        }
+
+      public function inserttoStore($material_desc,$unit,$quantity,$unit_price,$total_price,$remark){
+          $dt = new DateTime();
+          $delivered_date = $dt->format('Y-m-d');
+          $connection = $this->db->connect();
+          $result = mysqli_query($connection,"SELECT * FROM store WHERE material_description='$material_desc'");
+          $num_of_rows=mysqli_num_rows($result);
+          if($num_of_rows == 0){
+          mysqli_query($connection,"INSERT into store values('','$material_desc','$unit','$quantity','$unit_price','$total_price','$remark')");
+          }else{
+            while($row = mysqli_fetch_object($result)){
+                $quant=$row->quantity;
+                $quant=intval($quant);
+                $quantity=intval($quantity);
+                $currentQuantity=$quant+$quantity;                
+               mysqli_query($connection,"UPDATE store SET quantity='$currentQuantity' where material_description='$material_desc'");
+               mysqli_query($connection,"DELETE FROM requested_materials WHERE material_description = '$material_desc'");
+
+          }
+        }
+
+         }
+         
+        //record of inventory 
+        public function inserttoInventoryRec($material_desc,$quantity,$delivered_by,$unit,$unit_price,$total_price,$remark){
+            $dt = new DateTime();
+            $delivered_date = $dt->format('Y-m-d');
+            $connection = $this->db->connect();
+            mysqli_query($connection,"INSERT into inventory_record values('','$material_desc','$delivered_date','$delivered_by','$unit','$quantity','$unit_price','$total_price','$remark')");
+            mysqli_query($connection,"DELETE FROM approved_pr WHERE material_description = '$material_desc'"); 
+
+        }
+      //draw material from store
+      
+      public function drawmaterialfromStore($material_desc,$quantity){
+          $connection = $this->db->connect();
+          $result = mysqli_query($connection,"SELECT * from store WHERE material_description='$material_desc'");
+          $num_of_rows=mysqli_num_rows($result);
+
+          if($num_of_rows !=0){
+            $res=mysqli_query($connection,"SELECT * FROM store where material_description = '$material_desc'");
+            while($row = mysqli_fetch_object($res)){
+              $quant=$row->quantity;
+              $quant=intval($quant);
+              $quantity=intval($quantity);
+              $currentQuantity=$quant-$quantity; 
+              mysqli_query($connection,"UPDATE store SET quantity ='$currentQuantity' where material_description='$material_desc'");
+              mysqli_query($connection,"DELETE FROM requested_materials WHERE material_description = '$material_desc'");
+             } 
+    
+          }
+        }
+
+
+     public function declineRequest($type){
+         $connection = $this->db->connect();
+         $result = mysqli_query($connection,"DELETE FROM purchase_requests Where type = '$type'");
+        }  
+
+     public function store(){
+         $connection = $this->db->connect();
+         $result = mysqli_query($connection,"SELECT * FROM store");
+         return $result;
+     } 
+     
+     //draw inventory
+     public function drawInventory($type,$standard,$currentAmount){
+        $connection = $this->db->connect();
+        mysqli_query($connection,"UPDATE purchase_requests SET amount = $currentAmount WHERE type = '$type' ");    
+      
+      }
+
+     //to notify inventory manager
+     public function numInventory(){
+        $connection = $this->db->connect();
+        $result = mysqli_query($connection,"SELECT * from purchase_requests WHERE acceptance_status = 0");
+        return $result;
+    }
+   
+    //approved purchasing request
+
+    public function numApr(){
+        $connection = $this->db->connect();
+        $result = mysqli_query($connection,"SELECT * FROM approved_pr");
+        return $result;
+    }
+    
+    //number of material request
+
+    public function no_of_mRequest(){
+        $connection = $this->db->connect();
+        $result = mysqli_query($connection,"SELECT * FROM requested_materials");
+        return $result;
+    }
+    //inventory record
+
+    public function inventoryRecord(){
+        $connection = $this->db->connect();
+        $result = mysqli_query($connection,"SELECT * FROM inventory_record");
+        return $result;
+    }
+    //requested material
+
+    public function requestedMaterial(){
+        $connection = $this->db->connect();
+        $result = mysqli_query($connection,"SELECT * FROM requested_materials");
+        return $result;
+    }
+   //approved purchasing request
+
+   public function approvedPr(){
+       $connection = $this->db->connect();
+       $result = mysqli_query($connection,"SELECT * FROM approved_pr");
+       return $result;
+   }
+
+  }  
+
 
 ?>
